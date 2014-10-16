190bedd937d4e802b208167fda24687cef0d7f14
Takashi Nakagawa <tak1240@gmail.com>
2014-10-16 16:33:50 +0900

    solved with hacky way
    because github used pjax in own way and can’t hold event correctly

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,19 +1,27 @@
 'use strict';
 
-var referrer = document.referrer || '';
-var isLocationValid = !!(location.href.indexOf('commit') !== -1)
-var isReferrerValid = !!(!!referrer && referrer.indexOf('github.com') !== -1)
-var files = document.querySelectorAll('.file');
-var alert = '<div style="position:fixed;bottom:0px;left:0px;">Focusing via focus history</div>'
+var script = document.createElement('script');
+script.innerHTML = "var alert = '<div style=position:fixed;bottom:0px;left:0px;>Focusing via focus history</div>';"
 
-if(isLocationValid && files.length != 0 && isReferrerValid) {
-  var filePath;
-  for(var i = 0, file; file = files[i]; i++){
-    var path = file.querySelector('div[data-path]');
-    filePath = path.getAttribute('data-path');
-    if(referrer.indexOf(filePath) === -1){
-      file.setAttribute('style', 'display:none;');
-    }
-  }
-  document.body.innerHTML += alert;
-}
+script.innerHTML += "$.hashChange(hideNoise);"
+
+script.innerHTML +=  "function hideNoise() {"
+script.innerHTML +=  "var referrer = document.referrer;"
+script.innerHTML +=  "var isLocationValid = !!(location.href.indexOf('commit') !== -1);"
+script.innerHTML +=  "var isReferrerValid = !!(!!referrer && referrer.indexOf('github.com') !== -1);"
+script.innerHTML +=  "var files = document.querySelectorAll('.file');"
+script.innerHTML +=  "if(isLocationValid && files.length != 0 && isReferrerValid) {"
+script.innerHTML +=  "var filePath;"
+script.innerHTML +=  "for(var i = 0, file; file = files[i]; i++){"
+script.innerHTML +=  "var path = file.querySelector('div[data-path]');"
+script.innerHTML +=  "filePath = path.getAttribute('data-path');"
+script.innerHTML +=  "if(referrer.indexOf(filePath) === -1){"
+script.innerHTML +=  "file.setAttribute('style', 'display:none;');"
+script.innerHTML +=  "}"
+script.innerHTML +=  "}"
+script.innerHTML +=  "document.body.innerHTML += alert;"
+script.innerHTML +=  "}"
+script.innerHTML +=  "}"
+
+console.log(script);
+document.body.appendChild(script);
